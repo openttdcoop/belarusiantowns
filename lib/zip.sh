106960c460a3ef9c7a25fd1a580bb817bda17976
Ammler <ammler@openttdcoop.org>
2010-09-06 16:35:33 +0200

    Fix: don't error, if no bundle dir

--- lib/zip.sh
+++ lib/zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -r $FILENAME
+rm -r $FILENAME 2>/dev/null
 mkdir $FILENAME
 cp $FILENAME.grf readme.txt changelog.txt $FILENAME/
 7za a -l -tzip -mx=9 $FILENAME-$VERSION.zip $FILENAME
